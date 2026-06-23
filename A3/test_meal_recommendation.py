34719aa56926ca40ccd646e5fb92e86ec2711a18
Lana-1006 <laab3490@bht-berlin.de>
2026-06-23 14:48:06 +0200

    Create test_meal_recommendation.py

---
+++ A3/test_meal_recommendation.py
@@ -0,0 +1,20 @@
+from unittest.mock import Mock
+from meal_recommendation import MealRecommendationService
+
+
+def test_recommend_salad_when_weather_is_warm():
+    weather_service = Mock()
+    weather_service.get_weather.return_value = "warm"
+
+    service = MealRecommendationService(weather_service)
+
+    assert service.recommend_meal() == "Salat"
+
+
+def test_recommend_soup_when_weather_is_cold():
+    weather_service = Mock()
+    weather_service.get_weather.return_value = "cold"
+
+    service = MealRecommendationService(weather_service)
+
+    assert service.recommend_meal() == "Suppe"
